3d1e525e0fdf1d3138fc7b03290dae8ab6e45f44
Alfonso Pruneda <alfonso.pru@outlook.com>
2024-07-11 15:36:27 -0600

    small changes to make particle file more coherent

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Canvas from './Canvas'
-import { particles } from '../particles'
+import { heroBg } from '../particles'
 
 export default function Hero({homeRef, aboutRef}) {
     return (
         <div className='hero' ref={homeRef}>
-        <Canvas canvas={particles} className="particles-container" id="hero-particles"/>
+        <Canvas canvas={heroBg} className="particles-container" id="hero-particles"/>
         <div className='heading'>
                 <h1 className='heading-line-1'>Hello, I'm <span>Fonzie</span>.</h1>
                 <h1 className='heading-line-2'>I'm a Frontend Developer.</h1>
