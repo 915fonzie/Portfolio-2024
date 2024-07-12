ef8886e0424aff1277f764cdaacb2377b4b36adc
Alfonso Pruneda <alfonso.pru@outlook.com>
2024-07-11 21:28:19 -0600

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 export default function Contact({ contactRef }) {
     
     function handleSubmit(e) {
-        e.preventDefault()
+        // e.preventDefault()
     }
 
     return (
         <section className='contact' ref={contactRef}>
             <h1 className='section-heading'>Contact</h1>
             <p className='contact-text'>Have a question or want to work together? Leave your details and I'll get back to you as soon as possible.</p>
-            <form name='contact' method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+            <form name='contact' method="post">
                 <input type="hidden" name="form-name" value="contact" />
                 <input type='text' name='name' placeholder='Name'/>
                 <input type='email' name='email' placeholder='Email' />
