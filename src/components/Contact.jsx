48ebf0e596c9cfd5ff5526adbe24ed03361458cc
Alfonso Pruneda <alfonso.pru@outlook.com>
2024-07-11 21:14:07 -0600

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,12 +10,11 @@ export default function Contact({ contactRef }) {
         <section className='contact' ref={contactRef}>
             <h1 className='section-heading'>Contact</h1>
             <p className='contact-text'>Have a question or want to work together? Leave your details and I'll get back to you as soon as possible.</p>
-            <form name='contact' method="POST" data-netlify="true">
+            <form name='contact' method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+                <input type="hidden" name="form-name" value="contact" />
                 <input type='text' name='name' placeholder='Name'/>
                 <input type='email' name='email' placeholder='Email' />
                 <textarea name='message' placeholder='Message' />
-                <input type="hidden" name="form-name" value="contact" />
-
                 <button type='submit' onClick={(event) => handleSubmit(event)}>SUBMIT</button>
             </form>
         </section>
